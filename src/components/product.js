7abfc5365712846127fda683b777cf5113182c5b
Param Harrison <paramanantham@live.com>
2020-05-11 14:31:24 +0300

    products and product styles

--- src/components/product.js
+++ src/components/product.js
@@ -1,14 +1,49 @@
 import React from "react";
+import { Flex, Box, Heading, Text, Badge, Divider } from "@chakra-ui/core";
+
+const Categories = ({ tags }) => {
+  return (
+    <Box ml="auto">
+      {tags.map(tag => {
+        return (
+          <Badge
+            key={tag}
+            ml="1"
+            py="1"
+            px="2"
+            variantColor="purple"
+            textTransform="lowercase"
+          >
+            {tag}
+          </Badge>
+        );
+      })}
+    </Box>
+  );
+};
 
 const Product = ({ product }) => {
   return (
-    <li key={product.name}>
-      <h3>{product.name}</h3>
-      <p>{product.brand}</p>
-      <div>
-        <strong>${product.price}</strong>
-      </div>
-    </li>
+    <Flex
+      border="1px solid"
+      borderColor="gray.700"
+      borderRadius="4px"
+      flexDirection="column"
+    >
+      <Box p="4">
+        <Heading as="h3" fontSize="xl">
+          {product.name}
+        </Heading>
+        <Text>{product.brand}</Text>
+      </Box>
+      <Divider />
+      <Flex alignItems="center" px="4" pt="1" pb="3">
+        <Box>
+          <Text as="strong">${product.price}</Text>
+        </Box>
+        <Categories tags={product.categories} />
+      </Flex>
+    </Flex>
   );
 };
 
--- src/components/products.js
+++ src/components/products.js
@@ -1,13 +1,29 @@
 import React from "react";
 import Product from "./product";
+import { Grid, Box, Heading, Text } from "@chakra-ui/core";
 
 const Products = ({ products }) => {
   return (
-    <ul>
-      {products.map(({ node: product }) => (
-        <Product product={product} key={product.name} />
-      ))}
-    </ul>
+    <Box as="section" maxW="720px" mx="auto" my="3" p="3">
+      <Box textAlign="center" maxW="600px" my="60px" mx="auto">
+        <Heading as="h2" fontSize="2xl" textTransform="uppercase" mb="3">
+          Ecoshop products for you
+        </Heading>
+        <Text fontSize="lg">
+          See our shop product lists. Explore and buy if you can. Also learn
+          from our ecommerce site on how not to create an ecommerce product list
+          <Text as="span" role="img" aria-label="smile">
+            {" "}
+            😁
+          </Text>
+        </Text>
+      </Box>
+      <Grid templateColumns="repeat(2, 1fr)" gap={6}>
+        {products.map(({ node: product }) => (
+          <Product product={product} key={product.name} />
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
