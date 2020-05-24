8dbc6a6de779f27a7d9854455d7dbee8ce62cd97
Param Harrison <paramanantham@live.com>
2020-05-24 21:08:15 +0300

    feat: stripe key from env variables

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,13 +6,15 @@
 
 // You can delete this file if you're not using it
 
+require("dotenv").config();
+
 import React from "react";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
 import CartProvider from "./src/context/cart";
 
-const stripePromise = loadStripe("pk_test_FqfOKoybWF1MEQNRNbk5GWuw00Qgtn3SLM");
+const stripePromise = loadStripe(`${process.env.GATSBY_STRIPE_KEY}`);
 
 export const wrapRootElement = ({ element }) => (
   <Elements stripe={stripePromise}>
