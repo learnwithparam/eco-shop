7e909caf27d3cd00784884295a04bffc381c2e80
Param Harrison <paramanantham@live.com>
2020-05-24 22:04:33 +0300

    feat: enable manifest and offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,18 +22,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Eco shop by Param`,
+        short_name: `Ecoshop`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#1A202C`,
+        theme_color: `#ecc94b`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
